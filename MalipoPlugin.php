d2f2aff8e1d97f735cddf28c5953e71313cc65ca
Otuoma <otuomasp>
2024-02-02 13:28:22 +0300

    first commit

---
+++ MalipoPlugin.php
@@ -0,0 +1,295 @@
+<?php
+/**
+ * @file plugins/paymethod/malipo/MalipoPlugin.php
+ *
+ * Copyright (c) 2024 HyperLink Consulting LTD
+ * Copyright (c) 2024 Otuoma Sanya
+ * Distributed under the GNU GPL v3.
+ * Resource: https://stripe.com/docs/payments/accept-a-payment?platform=web&ui=embedded-checkout
+ * @class MalipoPlugin
+ * @brief Mpesa payment plugin
+ */
+namespace APP\plugins\paymethod\malipo;
+
+use APP\core\Application;
+use APP\plugins\paymethod\malipo\Utilities;
+use APP\template\TemplateManager;
+use PKP\db\DAORegistry;
+use PKP\form\Form;
+use PKP\plugins\Hook;
+use PKP\plugins\PaymethodPlugin;
+
+class MalipoPlugin extends PaymethodPlugin {
+
+    public function register($category, $path, $mainContextId = NULL): bool{
+
+        $success = parent::register($category, $path, $mainContextId);
+
+        if ($success && $this->getEnabled()) {
+            $this->addLocaleData();
+            Hook::add('Form::config::before', [$this, 'addSettings']);
+
+            $request = Application::get()->getRequest();
+
+            $templateMgr = TemplateManager::getManager($request);
+            $mpesaLogoUrl = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/images/mpesa-logo.png';
+            $stripeLogoUrl = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/images/stripe-logo.png';
+            $stripeUtilsUrl = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/js/stripeUtils.js';
+
+            // Create PaymentIntent and add Stripe.js only if we have stripe as gateway
+            $reqMethod = $this->getRequest()->getRequestMethod();
+            $gateway = $this->getRequest()->getUserVar('gateway');
+            if ($reqMethod == 'POST' && isset( $gateway ) && $gateway == 'stripe'){
+                $templateMgr->assign('stripePublishableKey', $mpesaLogoUrl);
+                $templateMgr->addJavaScript('stripeJs', 'https://js.stripe.com/v3/');
+                $templateMgr->addJavaScript('stripeUtils', $stripeUtilsUrl);
+            }
+
+            $templateMgr->assign('mpesaLogoUrl', $mpesaLogoUrl);
+            $templateMgr->assign('stripeLogoUrl', $stripeLogoUrl);
+            $styleUrl = $request->getBaseUrl() . '/' . $this->getPluginPath() . '/css/style.css';
+            $templateMgr->addStyleSheet('malipoStyles', $styleUrl);
+        }
+
+        return $success;
+    }
+
+
+    /**
+     * Get the payment form for this plugin.
+     *
+     * @param \PKP\context\Context $context
+     * @param \PKP\payment\QueuedPayment $queuedPayment
+     *
+     * @return \PKP\form\Form
+     */
+    public function getPaymentForm($context, $queuedPayment): Form {
+
+        $reqMethod = $this->getRequest()->getRequestMethod();
+        $submitUrl = $this->getRequest()->url(null, 'payment', 'plugin', [$this->getName(), 'daraja-callback'], ['queuedPaymentId' => $queuedPayment->getId()]);
+        $stripeSubmitUrl = null;
+
+        switch ($reqMethod){
+            case 'POST': // retrieve form data
+
+                $gateway = $this->getRequest()->getUserVar('gateway');
+
+                if ($gateway == 'mpesa'){
+                    $paymentForm = new Form($this->getTemplateResource('mpesa_request_payment.tpl'));
+                }else if ($gateway == 'stripe'){
+                    $paymentForm = new Form($this->getTemplateResource('init_payment_intent.tpl'));
+                    $stripeSubmitUrl = $this->getRequest()->url(null, 'payment', 'plugin', [$this->getName(), 'init-payment-intent', $queuedPayment->getId()]);
+                }
+                break;
+            default:
+                $paymentForm = new Form($this->getTemplateResource('malipo_choose_gateway.tpl'));
+        }
+
+        $paymentManager = Application::getPaymentManager($context);
+
+        $paymentForm->setData([
+            'itemName' => $paymentManager->getPaymentName($queuedPayment),
+            'itemAmount' => $queuedPayment->getAmount() > 0 ? $queuedPayment->getAmount() : null,
+            'itemCurrencyCode' => $queuedPayment->getAmount() > 0 ? $queuedPayment->getCurrencyCode() : null,
+            'queuedPaymentId' => $queuedPayment->getId(),
+            'pluginName' => $this->getName(),
+            'submitUrl' => $submitUrl,
+            'reqMethod' => $reqMethod,
+            'contextPath' => $context->getPath(),
+            'stripeSubmitUrl' => $stripeSubmitUrl,
+        ]);
+        return $paymentForm;
+    }
+
+    /**
+     * Must be implemented in a child class to save payment settings and attach updated data to the response
+     */
+    public function saveSettings(string $hookname, array $args): void {
+        $slimRequest = $args[0];
+        $request = $args[1];
+        $updatedSettings = $args[3];
+
+        $allParams = $slimRequest->getParsedBody();
+        $saveParams = [];
+        foreach ($allParams as $param => $val) {
+            switch ($param) {
+                case 'stripeTestMode':
+                case 'mpesaTestMode':
+                    $saveParams[$param] = $val === 'true';
+                    break;
+                case 'mpesaConsumerId':
+                case 'mpesaPassKey':
+                case 'mpesaBusinessShortCode':
+                case 'mpesaConsumerSecret':
+                case 'stripePublishableKey':
+                case 'stripeSecretKey':
+                    $saveParams[$param] = (string) $val;
+                    break;
+            }
+        }
+        $contextId = $request->getContext()->getId();
+        foreach ($saveParams as $param => $val) {
+            $this->updateSetting($contextId, $param, $val);
+            $updatedSettings->put($param, $val);
+        }
+    }
+
+    public function addSettings($hookName, $form): void {
+        // TODO: Implement saveSettings() method.
+        import('lib.pkp.classes.components.forms.context.PKPPaymentSettingsForm'); // Load constant
+
+        $context = Application::get()->getRequest()->getContext();
+        if ($form->id !== FORM_PAYMENT_SETTINGS || !$context) {
+            return;
+        }
+        $form->addGroup([
+            'id' => 'mpesaPayment',
+            'label' => 'MPESA Fee Payment',
+            'showWhen' => 'paymentsEnabled',
+        ])
+            ->addField(new \PKP\components\forms\FieldOptions('mpesaTestMode', [
+                'label' => 'Test mode',
+                'options' => [
+                    ['value' => true, 'label' => __('common.enable')]
+                ],
+                'value' => (bool) $this->getSetting($context->getId(), 'mpesaTestMode'),
+                'groupId' => 'mpesaPayment',
+            ]))
+            ->addField(new \PKP\components\forms\FieldText('mpesaConsumerId', [
+                'label' => 'Consumer ID',
+                'value' => $this->getSetting($context->getId(), 'mpesaConsumerId'),
+                'groupId' => 'mpesaPayment',
+            ]))
+            ->addField(new \PKP\components\forms\FieldText('mpesaConsumerSecret', [
+                'label' => 'Consumer Secret',
+                'value' => $this->getSetting($context->getId(), 'mpesaConsumerSecret'),
+                'groupId' => 'mpesaPayment'
+            ]))
+            ->addField(new \PKP\components\forms\FieldText('mpesaPassKey', [
+                'label' => 'Pass Key',
+                'value' => $this->getSetting($context->getId(), 'mpesaPassKey'),
+                'groupId' => 'mpesapayment',
+            ]))
+            ->addField(new \PKP\components\forms\FieldText('mpesaBusinessShortCode', [
+                'label' => 'Business Short Code',
+                'value' => $this->getSetting($context->getId(), 'mpesaBusinessShortCode'),
+                'groupId' => 'mpesaPayment',
+            ]));
+
+        $form->addGroup([
+            'id' => 'stripePayment',
+            'label' => 'Stripe Fee Payment',
+            'showWhen' => 'paymentsEnabled',
+        ])
+            ->addField(new \PKP\components\forms\FieldOptions('stripeTestMode', [
+                'label' => 'Test mode',
+                'options' => [
+                    ['value' => true, 'label' => __('common.enable')]
+                ],
+                'value' => (bool) $this->getSetting($context->getId(), 'stripeTestMode'),
+                'groupId' => 'stripePayment',
+            ]))
+            ->addField(new \PKP\components\forms\FieldText('stripePublishableKey', [
+                'label' => 'Publishable Key',
+                'value' => $this->getSetting($context->getId(), 'stripePublishableKey'),
+                'groupId' => 'stripePayment',
+            ]))
+            ->addField(new \PKP\components\forms\FieldText('stripeSecretKey', [
+                'label' => 'Secret Key',
+                'value' => $this->getSetting($context->getId(), 'stripeSecretKey'),
+                'groupId' => 'stripePayment',
+            ]));
+    }
+
+    /**
+     * Handle incoming requests/notifications
+     *
+     * @param array $args
+     * @param \APP\core\Request $request
+     */
+    public function handle($args, $request){
+        $journal = $request->getJournal();
+        $queuedPaymentDao = DAORegistry::getDAO('QueuedPaymentDAO');
+        $paymentManager = Application::getPaymentManager($journal);
+        $queuedPaymentId = $args[1];
+        $templateMgr = TemplateManager::getManager($request);
+
+        $darajaCallback = $this->getRequest()->url(null, 'payment', 'plugin', [$this->getName(), 'daraja-callback', $queuedPaymentId], []);
+        $action = $args[0];
+
+        $queuedPayment = $queuedPaymentDao->getById($queuedPaymentId);
+
+        if (!$queuedPayment) {
+            throw new \Exception("Invalid queued payment ID {$queuedPaymentId}!");
+        }
+
+        $utilities = new Utilities($this);
+
+        if ($action == 'init-payment-intent'){
+
+            $stripeClientSecret = $utilities->initPaymentIntent($queuedPayment);
+            $publishableKey = $this->getSetting($this->getCurrentContextId(), 'stripePublishableKey');
+
+            echo json_encode(
+                array(
+                    'clientSecret' => $stripeClientSecret,
+                    'publishableKey' => $publishableKey,
+                )
+            );
+        }
+
+        if ($action == 'stripe-callback'){
+            try {
+                // retrieve JSON from POST body
+                $jsonStr = file_get_contents('php://input');
+                $jsonObj = json_decode($jsonStr);
+
+                $session = $stripe->checkout->sessions->retrieve
+                ($jsonObj->session_id);
+
+                echo json_encode(['status' => $session->status, 'customer_email' => $session->customer_details->email]);
+                http_response_code(200);
+            } catch (Error $e) {
+                http_response_code(500);
+                echo json_encode(['error' => $e->getMessage()]);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get the display name for this plugin.
+     *
+     * @return string
+     */
+    public function getDisplayName(): string{
+        return 'Malipo Payments';
+    }
+
+    public function getName(): string{
+        return 'MalipoPayments';
+    }
+
+    /**
+     * Get a description of this plugin.
+     *
+     * @return string
+     */
+    public function getDescription(): string {
+        // TODO: Implement getDescription() method.
+        return "Integrates Stripe and MPesa Payment options into OJS >=3.4";
+    }
+
+    /*
+    *  $payMethod is either "mpesa" or "stripe"
+    */
+    public function isTestMode($context, $payMethod): bool {
+
+        if (!$context) return false;
+
+        if ($this->getSetting($context->getId(), $payMethod.'TestMode') == '1') {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Utilities.php
@@ -0,0 +1,223 @@
+<?php
+
+/**
+ * @file plugins/paymethod/malipo/Utilities.php
+ *
+ * Copyright (c) 2024 HyperLink DSL
+ * Copyright (c) 2024 Otuoma Sanya
+ * Distributed under the GNU GPL v3.
+ * @class Utilities
+ * @brief Mpesa payment page
+ */
+
+namespace APP\plugins\paymethod\malipo;
+
+use APP\core\Application;
+use Error;
+use PKP\payment\QueuedPayment;
+
+require_once 'vendor/autoload.php';
+
+class Utilities {
+
+    public MalipoPlugin $plugin;
+    /**
+     * @var mixed|\PKP\cache\generic_cache_miss|null
+     */
+    private string $stripeSecretKey;
+    private \Stripe\StripeClient $stripeClient;
+
+    public function __construct(MalipoPlugin $plugin){
+        $this->plugin = $plugin;
+        $this->stripeSecretKey = $plugin->getSetting($this->plugin->getCurrentContextId(), 'stripeSecretKey');
+
+        $this->stripeClient = new \Stripe\StripeClient(["api_key"=>$this->stripeSecretKey]);
+
+    }
+
+    public function initPaymentSession(QueuedPayment $queuedPayment): bool|string {
+
+        $callbackUrl = $this->plugin->getRequest()->url(null, 'payment', 'plugin', [$this->plugin->getName(), 'stripe-callback', $queuedPayment->getId()]);
+        $paymentManager = Application::getPaymentManager($this->plugin->request->getContext());
+
+        $stripeSession = $this->stripeClient->checkout->sessions->create([
+            'return_url' => $callbackUrl,
+            'line_items' => [
+                [
+                    'quantity'   => 1,
+                    'price_data' => [
+                        'currency'     => $queuedPayment->getCurrencyCode(),
+                        'unit_amount'  => $queuedPayment->getAmount() * 100,
+                        'product_data' => [
+                            'name' => $paymentManager->getPaymentName($queuedPayment),
+                        ]
+                    ]
+
+                ],
+            ],
+            'mode' => 'payment',
+            'ui_mode' => 'embedded',
+        ]);
+
+        return  $stripeSession->client_secret;
+    }
+
+    public function getSessionStatus(){
+        try {
+            // retrieve JSON from POST body
+            $jsonStr = file_get_contents('php://input');
+            $jsonObj = json_decode($jsonStr);
+
+            $session = $this->stripeClient->checkout->sessions->retrieve
+            ($jsonObj->session_id);
+
+            return json_encode([
+                'status' => $session->status,
+                'customer_email' => $session->customer_details->email
+            ]);
+
+        } catch (Error $e) {
+
+            return json_encode([
+                'status' => 'Failed',
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function STKPush($BusinessShortCode, $Amount, $PartyA, $PartyB, $PhoneNumber, $CallBackURL, $AccountReference, $TransactionDesc): bool|string
+    {
+
+        $context = Application::get()->getRequest()->getContext();
+
+        $testMode = $this->plugin->isTestMode($context);
+
+        if( $testMode ){
+            $reqUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+            $token = $this->generateSandBoxToken();
+        }else{
+            $reqUrl = 'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+            $token = $this->generateLiveToken();
+        }
+
+        $timestamp = '20' . date(    "ymdhis");
+        $passKey = $this->plugin->getSetting($context, "mpesaPassKey");
+        $password= base64_encode($BusinessShortCode.$passKey.$timestamp);
+        $transactionType = "CustomerPayBillOnline";
+
+        $reqHeaders = [
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+
+        $reqBody = [
+            'BusinessShortCode' => $BusinessShortCode,
+            'Password' => $password,
+            'Timestamp' => $timestamp,
+            'TransactionType' => $transactionType,
+            'Amount' => $Amount,
+            'PartyA' => $PartyA,
+            'PartyB' => $PartyB,
+            'PhoneNumber' => $PhoneNumber,
+            'CallBackURL' => $CallBackURL,
+            'AccountReference' => $AccountReference,
+            'TransactionDesc' => $TransactionDesc,
+        ];
+        $httpClient = Application::get()->getHttpClient();
+
+        $resp = $httpClient->request('POST', $reqUrl, [
+            'headers' => $reqHeaders,
+            'json' => $reqBody,
+        ]);
+
+        return $resp->getBody();
+    }
+
+    public function querySTKStatus($context, $checkoutRequestID){
+
+        try {
+            $token = $this->plugin->isTestMode($context)
+                ? $this->generateSandBoxToken()
+                : $this->generateLiveToken();
+
+            $testUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query';
+            $liveUrl = 'https://api.safaricom.co.ke/mpesa/stkpushquery/v1/query';
+            $businessShortCode = $this->plugin->getSetting($context, 'mpesaBusinessShortCode');
+
+            $reqUrl = $this->plugin->isTestMode($context) ? $testUrl : $liveUrl;
+
+            $timestamp = '20' . date(    "ymdhis");
+            $passKey = $this->plugin->getSetting($context, 'mpesaPassKey');
+            $password= base64_encode($businessShortCode.$passKey.$timestamp);
+            $reqHeaders = [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $token
+            ];
+
+            $reqBody = [
+                'BusinessShortCode' => $businessShortCode,
+                'Password' => $password,
+                'Timestamp' => $timestamp,
+                'CheckoutRequestID' => $checkoutRequestID
+            ];
+            $httpClient = Application::get()->getHttpClient();
+            $resp = $httpClient->request('POST', $reqUrl, [
+                'headers' => $reqHeaders,
+                'json' => $reqBody,
+            ]);
+
+            return $resp->getBody();
+
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+            return $e->getMessage();
+        }
+    }
+    public function generateLiveToken(){
+
+        $context = Application::get()->getRequest()->getContext();
+
+        $consumerId = $this->plugin->getSetting($context->getId(), 'consumerId');
+        $consumerSecret = $this->plugin->getSetting($context->getId(), 'consumerSecret');
+
+        if(!isset($consumerId)||!isset($consumerSecret)){
+            die("LIVE - please declare the consumer key and consumer secret as defined in the documentation");
+        }
+        $reqUrl = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+
+        $credentials = base64_encode($consumerId . ':' . $consumerSecret);
+        $httpClient = Application::get()->getHttpClient();
+        $response = $httpClient->request('GET', $reqUrl, [
+            'headers' => [ 'Authorization' => 'Basic ' . $credentials]
+        ]);
+
+        $respBody = $response->getBody()->getContents();
+        $decodedResp = json_decode($respBody);
+        return $decodedResp->access_token;
+
+    }
+    public function generateSandBoxToken(){
+
+        $context = Application::get()->getRequest()->getContext();
+
+        $consumerId = $this->plugin->getSetting($context->getId(), 'consumerId');
+        $consumerSecret = $this->plugin->getSetting($context->getId(), 'consumerSecret');
+
+        if(!isset($consumerId)||!isset($consumerSecret)){
+            die("SANDBOX - please declare the consumer key and consumer secret as defined in the documentation");
+        }
+        $reqUrl = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+
+        $credentials = base64_encode($consumerId . ':' . $consumerSecret);
+        $httpClient = Application::get()->getHttpClient();
+        $response = $httpClient->request('GET', $reqUrl, [
+            'headers' => [ 'Authorization' => 'Basic ' . $credentials]
+        ]);
+
+        $respBody = $response->getBody()->getContents();
+        $decodedResp = json_decode($respBody);
+        return $decodedResp->access_token;
+
+    }
+
+}
